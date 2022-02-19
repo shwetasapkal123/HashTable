697e8cacfdf870b35d77bdd28364afdd6f3ce3c1
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-02-19 23:57:00 +0530

    [shweta] Frequency of words in large paragraph phase

--- HashTable/HashTable/MyMapNode.cs
+++ HashTable/HashTable/MyMapNode.cs
@@ -71,6 +71,17 @@ namespace HashTable
                 }
             }
         }
+        public bool IsEmpty()
+        {
+            if (Getsize() <= 0)
+                return true;
+            else
+                return false;
+        }
+        public int Getsize()
+        {
+            return size;
+        }
     }
     public struct KeyValue<k, v>
     {
--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -11,21 +11,43 @@ namespace HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("WEL-COME In Hash Table");
-            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-            hash.Add("0", "To");
-            hash.Add("1", "be");
-            hash.Add("2", "or");
-            hash.Add("3", "not");
-            hash.Add("4", "to");
-            hash.Add("5", "be");
-
-            Console.WriteLine(hash.GetV("0"));
-            Console.WriteLine(hash.GetV("1"));
-            Console.WriteLine(hash.GetV("2"));
-            Console.WriteLine(hash.GetV("3"));
-            Console.WriteLine(hash.GetV("4"));
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(20);
+            Console.WriteLine(hash.IsEmpty());
+            hash.Add("0", "Paranoids");
+            hash.Add("1", "are");
+            hash.Add("2", "not");
+            hash.Add("3", "paranoid");
+            hash.Add("4", "because");
+            hash.Add("5", "they");
+            hash.Add("6", "are");
+            hash.Add("7", "paranoid");
+            hash.Add("8", "but");
+            hash.Add("9", "they");
+            hash.Add("10", "keep");
+            hash.Add("11", "putting");
+            hash.Add("12", "themselves");
+            hash.Add("13", "deliberately");
+            hash.Add("14", "into");
+            hash.Add("15", "paranoid");
+            hash.Add("16", "avoidable");
+            hash.Add("17", "situations");
             Console.WriteLine(hash.GetV("5"));
+            Console.WriteLine(hash.GetV("0"));
+            Console.WriteLine($"size is: {hash.Getsize()}");
+
+            string output = hash.GetV("3");
+            int count = 0;
+            for (int i = 0; i < hash.Getsize(); i++)
+            {
+                string temp = hash.GetV($"{i}");
+                if (output == temp)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"frequency of {output} is : {count}");
             Console.ReadLine();
         }
     }
+    
 }
